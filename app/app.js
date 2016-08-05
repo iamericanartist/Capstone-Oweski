ab0b979c3b43661afafd4ed84dedaf8413e9425d
Eric Denton <eric@iamericanartist.com>
2016-08-05 09:03:45 -0500

    mourning reversal

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var app = angular.module("OweSkiApp", ["ngRoute", "ngMaterial"]);                       // "OweSkiApp" is the name of our ng-app in HTML  /  ngRoute is being injected into our app here
+var app = angular.module("OweSkiApp", ["ngRoute"]);                       // "OweSkiApp" is the name of our ng-app in HTML  /  ngRoute is being injected into our app here
     app.constant('FirebaseURL', "https://oweski-180c3.firebaseio.com/");  // setting value of 'FirebaseURL' as the Firebase link / can be done for any variables
 
 
